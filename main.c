36f2155f527d2c055280ef158e26f34a9db1f4d6
Tom Andersen <tomandersen2000@gmail.com>
2016-11-10 09:35:26 -0800

    I am an idiot, color write mask was reset to zero

--- main.c
+++ main.c
@@ -1239,13 +1239,12 @@ void createGraphicsPipeline(struct Engine* engine)
         VK_COLOR_COMPONENT_B_BIT |
         VK_COLOR_COMPONENT_A_BIT;
     colorBlendAttachment.blendEnable = VK_FALSE;
-    /*colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
+    colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
     colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;
     colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
     colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
     colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
     colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
-    colorBlendAttachment.colorWriteMask = 0;*/
 
     memset(&colorBlendInfo, 0, sizeof(colorBlendInfo));
     colorBlendInfo.sType =
